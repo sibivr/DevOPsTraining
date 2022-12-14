87392aceafe6fa4306f36bd3a4b285d960d6f3c6
sibivr <sibivr@yahoo.com>
2022-12-14 17:22:20 +0000

    modified

--- tvs-script/mysql-backup.sh
+++ tvs-script/mysql-backup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+################
+##author - sibi
+##
+##############
 v_db=$1
 v_password=$(cat password)
 echo "mysqldump -hlwplabs.rds.aws.com -udbuser -p$v_password $v_db > /tmp/lwplabs.sql"
